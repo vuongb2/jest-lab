5d1a0470a3de08a014dd8b5b5806f043ef77fc46
vuongb2 <brandonvvuong@gmail.com>
2023-07-25 17:37:39 -0700

    added two jest tests

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -1,8 +1,30 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import userEvent from "@testing-library/user-event"
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test ('Render basic page components', () => {
+  render(<App />);
+  expect(screen.getByText("Hello World")).toBeInTheDocument();
+});
+
+// Class example
+test('renders learn react link', () => {
+  render(<App />);
+
+userEvent.click(screen.getByTestId("checkbox"));
+expect(screen.getByTestId("checkbox")).toBeChecked()
+});
+
+// My example
+test("If a button is clickable", () => {
+  render(<App />);
+  screen.debug();
+  const button = screen.getByRole('button');
+  userEvent.click(button);
 });
